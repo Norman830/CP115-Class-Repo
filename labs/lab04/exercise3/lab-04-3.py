93439066c0ca494f6e89e3512888d9231ceba88a
Norman830 <normandanishrajimin@gmail.com>
2026-07-15 11:46:54 +0800

    lab 04

---
+++ labs/lab04/exercise3/lab-04-3.py
@@ -0,0 +1,11 @@
+hours = int(input())
+if hours <= 2:
+    parkingfee = 0
+else:
+    if hours <= 5:
+        parkingfee = (hours - 2) * 2
+    else:
+        parkingfee = (3 * 2) + (hours - 5) * 3
+    if parkingfee > 30:
+        parkingfee = 30
+print(parkingfee)
