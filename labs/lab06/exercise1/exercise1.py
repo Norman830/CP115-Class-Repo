33f4422b31d865a5da3fb9cb0cd63911393096ec
Norman830 <normandanishrajimin@gmail.com>
2026-08-12 11:17:15 +0800

    lab06

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,4 +1,20 @@
-# Escape Characters Exercise
-# Print the receipt shown in the lab, using \n for new lines and \t for columns.
-# Calculate every total, subtotal, and tax in your code. Do not type the money
-# amounts in directly. Show every amount with exactly two decimal places.
+coffee = int(input("coffee:"))
+muffin = int(input("muffin:"))
+water = int(input("water:"))
+
+tcoffee = coffee * 3.50	
+tmuffin = muffin  * 2.10
+twater = water * 1.05
+subtotal = tcoffee + tmuffin + twater 
+tax = subtotal *0.06
+finaltotal = tax + subtotal
+
+print(f"========== RECEIPT ==========\n"
+      f"Item\tPrice\tQty\tTotal\n"
+      f"Coffee\t$3.50\t{coffee}\t{tcoffee:.2f}\n"
+      f"muffin\t$2.10\t{muffin}\t{tmuffin:.2f}\n"
+      f"water\t$1.05\t{water}\t{twater:.2f}\n"
+      f"------------------------------\n"
+      f"Subtotal\t\t{subtotal:.2f}\n"
+      f"Tax(6%)\t\t\t{tax:.2f}\n"
+      f"Total\t\t\t{finaltotal:.2f}")
