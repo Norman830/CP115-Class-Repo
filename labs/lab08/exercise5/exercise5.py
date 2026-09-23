94624dd3742bff6893f91202a65d7ae10ca1ccb9
Norman830 <normandanishrajimin@gmail.com>
2026-09-23 12:32:58 +0800

    lab 08

--- labs/lab08/exercise5/exercise5.py
+++ labs/lab08/exercise5/exercise5.py
@@ -2,6 +2,24 @@ main_course = input()
 drink = input()
 dessert = input()
 
+if main_course == "Chicken":
+    main_course = 10
+elif main_course == "Beef":
+    main_course = 12
+else:
+    main_course = 11
 
+if drink == "Soft Drink":
+    drink = 2
+else:
+    drink = 3
+
+if dessert == "Ice Cream":
+    dessert = 4
+else:
+    dessert = 5
+
+total = main_course + drink + dessert
+final_bill = total + (total * 0.10)
 
 print(f"{final_bill:.2f}")
--- labs/lab08/exercise6/exercise6.py
+++ labs/lab08/exercise6/exercise6.py
@@ -2,6 +2,13 @@ position = input()
 overtime_hours = int(input())
 is_weekend = input()
 
+if position == "Manager":
+    if is_weekend == "yes":
+        if overtime_hours <= 8:
+            overtime_pay = ((30 * 1.5) *  overtime_hours) + (overtime_hours * 5)
+        else:
+            overtime_pay = ((30 * .20 *  overtime_hours) + (overtime_hours * 5)
+
 
 
 print(overtime_pay)
